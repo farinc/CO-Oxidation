09b2493396754ba51f316f28231c58256594f16d
Forrest Ramirez <forrest.ram64@gmail.com>
2026-07-28 14:30:00 -0500

    Added a runner script

---
+++ run_tile_delta_set.py
@@ -0,0 +1,30 @@
+import subprocess as sp
+from pathlib import Path
+
+L = [8, 10, 12]
+Delta_scale = [1, 0.1, 0.01, 0.001, 0.0001, 0.00001]
+out_dir = Path('output/')
+
+for i,l in enumerate(L):
+    for delta_scale in Delta_scale:
+        prefix_job_name = f"tile_{l}_delta{i}"
+        command = [
+            "qsub",
+            "-N", prefix_job_name,
+            "-pe", "orte", "24",
+            "../submit_sweep_job.sh",
+            "--out", prefix_job_name,
+            "--no-kmc",
+            "--no-meanfield",
+            "--memkm-sites", f"{l}",
+            "--delta-scale", f"{delta_scale}",
+        ]
+
+        result = sp.run(
+            command,
+            check=True,
+            text=True,
+            capture_output=False,
+            cwd=out_dir
+        )
+
